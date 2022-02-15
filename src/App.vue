8a8b321016aebfdbae0be621804a687d74658bb5
anatosiy <anatosiy.8@gmail.com>
2022-02-16 00:21:25 +0200

    add files

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+  <div class="container pb-4">
+    <Main />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+
+export default {
+  components: {
+    Header,
+    Main,
+  },
+};
+</script>
